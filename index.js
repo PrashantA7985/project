4ecbc45929c0df2bb4fbdc039e0986410e5bcbac
PrashantA7985 <95953965+PrashantA7985@users.noreply.github.com>
2022-01-26 01:20:37 +0530

    Add files via upload
    adding js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ setInterval(() => {
     htime = d.getHours();
     mtime = d.getMinutes();
     stime = d.getSeconds();
-    hrotation = 30*htime + mtime/2;
+    hrotation = (30*htime)+(mtime/2);
     mrotation = 6*mtime;
     srotation = 6*stime;
 
